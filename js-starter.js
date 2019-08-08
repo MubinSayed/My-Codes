7c65689376ef7df517f662aedf554f9e672d5a95
Mubin Sayed <mbnsayed@gmail.com>
2019-08-08 15:13:25 +0530

    created js-strater file

---
+++ js-starter.js
@@ -0,0 +1,16 @@
+/* Starter Code for JS */
+var Welcome = function() {
+
+    var myFunctions = function(){
+        // code goes here
+    }
+    
+    
+    return {
+        init: function(){
+            myFunctions();
+        }
+    }
+}();
+
+Welcome.init();
